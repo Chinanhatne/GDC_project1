10b615015c2e6a60b2edaa32be0c15754a897442
Chinanhatne <nhatminhnguyen2292005@gmail.com>
2026-04-19 12:14:08 +0700

    timely

--- Assets/Scripts/Hook.cs
+++ Assets/Scripts/Hook.cs
@@ -1,20 +1,85 @@
 using UnityEngine;
+using UnityEngine.InputSystem; // For handling input actions
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Hook : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject mainCamera;
+    public Camera viewCamera;
+    public GameObject boat;
+    public GameObject hooker;
+    public Vector2 initialVelocity;
+    public LineRenderer lineRenderer;
+    public GameObject fishButton;
+    public GameObject Fish;
+    Vector2 mousePosition;
+    bool hooked = false;
+    bool fish = false;
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnReady()
     {
-        
+        mainCamera.GetComponent<Animator>().SetBool("Fish?",false);
+        fishButton.SetActive(true);
+        fish = false;
+    }
+
+    public void OnFish()
+    {
+        mainCamera.GetComponent<Animator>().SetBool("Fish?",true);
+        fishButton.SetActive(false);
+        hooker.transform.position = boat.transform.position;
+        fish = true;
+    }
+
+    public void OnHooked(GameObject fish)
+    {
+        mainCamera.GetComponent<Animator>().SetBool("Fish?",false);
+        hooked = true;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision);
+        SceneManager.LoadScene("TugOfwar");
+    }
+
+    public void OnMouseMove(InputAction.CallbackContext context)
+    {
+        mousePosition = context.ReadValue<Vector2>();
+        Vector2 UImiddle = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Vector2 mouseRelativePosition = mousePosition - UImiddle;
+        if(fish)
+        {
+            Debug.Log("mouse position" + mouseRelativePosition);
+            Vector3 cursor = viewCamera.ScreenToWorldPoint(mouseRelativePosition);
+            cursor.z = 0f;
+            hooker.transform.localPosition = cursor;
+        }
+
+    }
+
+    private bool preventMouseRelease = false; // Flag to prevent multiple triggers on mouse release
+
+    public void OnMouseClick(InputAction.CallbackContext context)
+    {
+        if(!preventMouseRelease)
+        {
+            preventMouseRelease = true;
+            Debug.Log("Mouse clicked");
+
+        }
+        else
+        {
+            preventMouseRelease = false; // Reset flag on mouse release
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
     {
         
     }
